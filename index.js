548b4cf45c70cbc4962a2acb1d5a8e8b0fee47ae
Adebanjo Timothy <adebanjotimothy3235@gmail.com>
2026-07-10 16:33:59 +0100

    add a basic hover effect to the grid

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const fragments = document.createDocumentFragment();
 
 for (let i = 0; i < 256; i++) {
     const createThem = document.createElement("div");
-    createThem.classList.add("flexDivs");
+    createThem.classList.add("createThem");
     createThem.style.width = "6.25%";
     createThem.style.height = "6.25%";
     createThem.style.backgroundColor = "beige";
     createThem.style.boxSizing = "border-box";
     createThem.style.border = "1px solid white";
     fragments.appendChild(createThem);
-}
+    
 container.appendChild(fragments);
 
+
+
+createThem.addEventListener('mouseover', e => {
+        createThem.style.backgroundColor = "black";
+    })
+}
 
 
